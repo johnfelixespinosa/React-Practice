74883e879a2cd742546647c70d71c344f6bf4da0
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-03-16 14:20:05 -0400

    Begin FileList code

--- github-mock/src/index.js
+++ github-mock/src/index.js
@@ -1,12 +1,97 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+// import Time from './time';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const FileList = ({ files }) => (
+  <table className='file-list'>
+    <tbody>
+      {files.map(file =>
+        <FileListItem key={file.id} file={file}/>
+      )}
+    </tbody>
+  </table>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+FileList.propTypes = {
+  files: PropTypes.array
+};
+
+const FileListItem = ({ file }) => (
+  <tr className='file-list-item'>
+    <td className='file-name'>{file.name}</td>
+  </tr>
+);
+
+FileListItem.propTypes = {
+  file: PropTypes.object.isRequired
+};
+
+function FileName({ file }) {
+  return (
+    <React.Fragment>
+      <FileIcon file={file}/>
+      <td className='file-name'>
+        {file.name}
+      </td>
+    </React.Fragment>
+  );
+};
+
+FileName.propTypes = {
+  file: PropTypes.object.isRequired
+};
+
+function FileIcon({ file }) {
+  let icon = 'fa-file-text-o';
+  if(file.type === 'folder') {
+    icon = 'fa-folder';
+  }
+
+  return (
+    <td className='file-icon'>
+      <i className={`fa ${icon}`}/>
+    </td>
+  );
+};
+
+FileIcon.propTypes = {
+  file: PropTypes.object.isRequired
+};
+
+const testFiles = [ 
+  {
+    id: 1,
+    name: 'src',
+    type: 'folder',
+    updated_at: "2016-07-11 21:24:00", 
+      latestCommit: {
+        message: 'Initial commit' 
+      }
+  }, 
+  {
+    id: 2,
+    name: 'tests',
+    type: 'folder',
+    updated_at: "2016-07-11 21:24:00", 
+      latestCommit: {
+        message: 'Initial commit' 
+      }
+  }, 
+  {
+    id: 3,
+    name: 'README',
+    type: 'file',
+    updated_at: "2016-07-18 21:24:00", 
+      latestCommit: {
+        message: 'Added a readme' 
+      }
+  }, 
+];
+
+ReactDOM.render(<FileList files={testFiles}/>, 
+  document.querySelector('#root')
+);
+
+export default FileList;
