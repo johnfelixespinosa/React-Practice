ca110418dd16c0f36f5e48ab13d9fc56c55ac5b9
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-03-25 15:13:02 -0400

    Chatter application practice

--- chatter/src/index.js
+++ chatter/src/index.js
@@ -1,23 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { combineReducers } from "redux"
+import messages from "./messages"
+import users from "./users"
 
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import chat from './reducers';
+const chat = combineReducers({
+  messages,
+  users
+});
 
-const store = createStore(chat)
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, 
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default chat
