b0466c9c1b746569fa0565b10780859580c79cc3
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-04-03 15:06:15 -0400

    Complete poster-mock application

--- poster-mock/src/index.js
+++ poster-mock/src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Poster({ posterInfo }) {
+  var { image, title, caption } = posterInfo;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div className='poster'>
+      <div className='image-container'>
+        <img alt='Poster' src={image}/>
+      </div>
+      <div className='title-container'>
+        <span className='title'>
+          {title}
+        </span>
+      </div>
+      <div className='caption'>
+        {caption}
+      </div>
+    </div>
+  );
+}
+Poster.propTypes = {
+  posterInfo: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired
+  }).isRequired
+};
+
+var posterInfo = {
+  image: "https://rawgit.com/gorangajic/react-icons/master/react-icons.svg",
+  title: "React",
+  caption: "The best thing since jQuery, probably."
+};
+
+ReactDOM.render(
+  <Poster posterInfo={posterInfo} />, 
+  document.getElementById('root')
+);
+
+
