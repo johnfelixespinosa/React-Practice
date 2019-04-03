75f59d4f3bc4ba3c3c8b3c6cd858582adbe62387
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-04-03 15:16:46 -0400

    Moved poster component to component folder

---
+++ poster-mock/src/components/poster.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import './poster.css';
+
+function Poster({ posterInfo }) {
+  var { image, title, caption } = posterInfo;
+
+  return (
+    <div className='poster'>
+      <div className='image-container'>
+        <img alt='Poster' src={image}/>
+      </div>
+      <div className='title-container'>
+        <span className='title'>
+          {title}
+        </span>
+      </div>
+      <div className='caption'>
+        {caption}
+      </div>
+    </div>
+  );
+}
+Poster.propTypes = {
+  posterInfo: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired
+  }).isRequired
+};
+
+var posterInfo = {
+  image: "https://rawgit.com/gorangajic/react-icons/master/react-icons.svg",
+  title: "React",
+  caption: "The best thing since jQuery, probably."
+};
+
+export default Poster
--- poster-mock/src/index.js
+++ poster-mock/src/index.js
@@ -1,34 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import './index.css';
-
-function Poster({ posterInfo }) {
-  var { image, title, caption } = posterInfo;
-
-  return (
-    <div className='poster'>
-      <div className='image-container'>
-        <img alt='Poster' src={image}/>
-      </div>
-      <div className='title-container'>
-        <span className='title'>
-          {title}
-        </span>
-      </div>
-      <div className='caption'>
-        {caption}
-      </div>
-    </div>
-  );
-}
-Poster.propTypes = {
-  posterInfo: PropTypes.shape({
-    image: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    caption: PropTypes.string.isRequired
-  }).isRequired
-};
+import Poster from './components/poster';
 
 var posterInfo = {
   image: "https://rawgit.com/gorangajic/react-icons/master/react-icons.svg",
@@ -37,8 +10,6 @@ var posterInfo = {
 };
 
 ReactDOM.render(
-  <Poster posterInfo={posterInfo} />, 
+  <Poster posterInfo={posterInfo}/>,
   document.getElementById('root')
 );
-
-
