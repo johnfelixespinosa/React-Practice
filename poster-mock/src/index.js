dca609cc941a8d2763749c98324d521221c0d46e
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-04-04 15:53:47 -0400

    poster-mock complete

--- poster-mock/src/index.js
+++ poster-mock/src/index.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 import Poster from './components/poster';
 
 var posterInfo = {
-  image: "https://rawgit.com/gorangajic/react-icons/master/react-icons.svg",
-  title: "React",
-  caption: "The best thing since jQuery, probably."
+  image: "https://www.universetoday.com/wp-content/uploads/2009/11/Apoll14_Shepard-e1435614695915.jpg",
+  title: "ACHIEVMENT",
+  caption: "This is one small step for man, one giant leap for manking. -Niel Armstrong"
 };
 
 ReactDOM.render(
