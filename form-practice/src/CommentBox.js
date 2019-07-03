4128a76a4f879afcc0f6c0c26edc7a2e9beb5f42
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-07-03 16:42:38 -0400

    Working comment box

---
+++ form-practice/src/CommentBox.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+// import CommentList from './CommentList';
+// import CommentForm from './CommentForm';
+
+class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      comment: '',
+      comments: ['First Comment', 'Second Comment', 'Third Comment'],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({comment: event.target.value});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState(state => {
+      const comments = [...state.comments, state.comment];
+
+      return {
+        comments,
+        comment: '',
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="comment-list-container">
+      {
+        this.state.comments.map((comment) =>
+          <li>{comment}</li>
+        )
+      }
+      
+      <form onSubmit = { this.handleSubmit } >
+        <label>
+          comment:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form >
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
